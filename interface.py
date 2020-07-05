0cdbc0a73a9b0379662efd07b2d1e38c890abfd6
Chen-Yue-Zhao <67790570+Chen-Yue-Zhao@users.noreply.github.com>
2020-07-06 01:56:22 +0800

    AddDisplayOfWarriors (require updates for towers)

--- interface.py
+++ interface.py
@@ -1,26 +1,25 @@
 import pgzrun
 import random
-from print_Warrior import *
+from print_Warrior import*
 from backstage import*
 
 
-turnID = 0 # 该命令所生效的回合
-
-# order_command = Command(turnID, CmdType, CmdStr)
+# 该命令所生效的回合
+turnID = 0
 
 
 # 显示页面大小
 WIDTH = 1200
 HEIGHT = 700
 
-BOX = Rect((0,250),(1200,250)) 
-# Rect(left, top, width, height) -> Rect 
+BOX = Rect((0, 250), (1200, 250))
+# Rect(left, top, width, height) -> Rect
 # Rect((left, top), (width, height)) -> Rect
 RED = 200, 0, 0
 
 
 # 创建造兵按钮 #长宽70
-# 创建造兵按钮 #长宽70
+
 warrior_up = Actor('小兵', (70, 385))
 archer_up = Actor('弓箭手', (166, 385))
 warrior_mid = Actor('小兵', (70, 510))
@@ -31,78 +30,67 @@ warrior_up.x = 70
 warrior_up.y = 385
 archer_up.x = 166
 archer_up.y = 385
-warrior_up.x = 70
-warrior_up.y = 385
-archer_up.x = 166
-archer_up.y = 385
 
+# 创建小兵对象
 
-
-
+Warrior_image_1 = Actor('小兵色块')
+Warrior_image_2 = Actor('小兵色块')
+image = ['小兵色块', '小兵色块']
+print(image)
 
 
 def draw():
     screen.clear()
     screen.fill("white")
     screen.blit('bk', (0, 0))
+
     # 兵营部分
     screen.blit('arsenal', (20, 190))
     screen.blit('soldier', (21, 280))
-    warrior_up.draw()    #(35, 350))
+    warrior_up.draw()  # (35, 350))
     archer_up.draw()   # (131, 350))
-    warrior_mid.draw() #(35, 350))
-    archer_mid.draw()  #(131, 350))
-    warrior_down.draw()  #(35, 350))
-    archer_down.draw()  #(131, 350))
+    warrior_mid.draw()  # (35, 350))
+    archer_mid.draw()  # (131, 350))
+    warrior_down.draw()  # (35, 350))
+    archer_down.draw()  # (131, 350))
+    # 路线部分
+    for k in range(1, 4):
+        for i in range(50):
+            screen.blit("purchase", (road[0][i][k][0], road[0][i][k][1]))
+    for k in range(1, 4):
+        for i in range(49):
+            screen.blit("purchase", (road[1][i][k][0], road[1][i][k][1]))
+    for k in range(1, 4):
+        for i in range(50):
+            screen.blit("purchase", (road[2][i][k][0], road[2][i][k][1]))
+
+    # 兵种部分
+    for r in range(3):
+        for w in w1[r]:
+            screen.blit(image[w.wtype - 2], (road[r][w.pos][w.wroute]))
+        for w in w2[r]:
+            screen.blit(image[w.wtype - 2], (road[r][w.pos][w.wroute]))
 
     '''
-    # 打印小兵
-    for i in range(69):
-        if road_1[0][i]['Knight'] != 0 or road_1[0][i]['Archer'] != 0:
-            for k in range (road_1[0][i]['Knight']):
-
-            for k in range (road_1[0][i]['Archer']):
-
-        if road_1[2][i]['Knight'] != 0 or road_1[2][i]['Archer'] != 0:
-            for k in range (road_1[2][i]['Knight']):
-
-            for k in range (road_1[2][i]['Archer']):
-        
-        if road_2[0][i]['Knight'] != 0 or road_2[0][i]['Archer'] != 0:
-            for k in range (road_2[0][i]['Knight']):
-
-            for k in range (road_2[0][i]['Archer']):
-        
-        if road_2[2][i]['Knight'] != 0 or road_2[2][i]['Archer'] != 0:
-            for k in range (road_2[2][i]['Knight']):
-
-            for k in range (road_2[2][i]['Archer']):
-
-    for i in range(49):
-        if road_1[1][i]['Knight'] != 0 or road_1[1][i]['Archer'] != 0:
-            for k in range (road_1[1][i]['Knight']):
-
-            for k in range (road_1[1][i]['Archer']):
-
-        if road_2[1][i]['Knight'] != 0 or road_2[1][i]['Archer'] != 0:
-            for k in range (road_2[1][i]['Knight']):
-
-            for k in range (road_2[1][i]['Archer']):        
+    for k in range (1, 4):
+        for i in range (50):
+            screen.blit("小兵色块",(road[0][i][k][0] + 5, road[0][i][k][1] + 5))
+    for k in range (1, 4):
+        for i in range (49):
+            screen.blit("弓箭手色块",(road[1][i][k][0] + 5, road[1][i][k][1] + 5))
+    for k in range (1, 4):
+        for i in range (50):
+            screen.blit("小兵色块",(road[2][i][k][0] + 5, road[2][i][k][1] + 5))
     '''
-    #打印小兵结束
-
-    
-    #screen.draw.text("A R S E N A L", (20, 200), color="pink", gcolor="VioletRed2", fontsize=50)
-
-    #建筑信息
 
 
+def on_mouse_down(pos):  # 造兵方式
+    order_command = Command(game.turnID, 0, [0])
 
-def on_mouse_down(pos): #造兵方式
     if warrior_up.collidepoint(pos):
         order_command.CmdType = 2
         order_command.CmdStr = [1]
-    elif archer_up.collidepoint(pos):
+    if archer_up.collidepoint(pos):
         order_command.CmdType = 3
         order_command.CmdStr = [1]
     elif warrior_mid.collidepoint(pos):
@@ -112,64 +100,12 @@ def on_mouse_down(pos): #造兵方式
         order_command.CmdType = 3
         order_command.CmdStr = [2]
     elif warrior_down.collidepoint(pos):
-       order_command.CmdType = 2
-       order_command.CmdStr = [3]
+        order_command.CmdType = 2
+        order_command.CmdStr = [3]
     elif archer_down.collidepoint(pos):
         order_command.CmdType = 3
         order_command.CmdStr = [3]
-    else:
-        order_command = []    # 这里需要商讨
-
-
-
-
-'''
-# 读取三条线路信息
-
-road_1 = [[], [], []] # 己方的小兵 第三阶的dict代表（兵种，个数）
-road_2 = [[], [], []] # 地方的小兵
-
-
-
-tower_1 
-tower_2 
-
-
-
-# 初始化路径信息
-def init_soldier():
-    for i in range(69):
-        road_1[0].append({'Knight': 0, 'Archer': 0})
-        road_1[2].append({'Knight': 0, 'Archer': 0})
-        road_2[0].append({'Knight': 0, 'Archer': 0})
-        road_2[2].append({'Knight': 0, 'Archer': 0})
-
-    for i in range(49):
-        road_1[1].append({'Knight': 0, 'Archer': 0})
-        road_2[1].append({'Knight': 0, 'Archer': 0})
-    print(road_1, "\n", road_2) 
-
-
-def save_soldier():
-    for i in range(3):
-        for k in range(len(w1[i])):
-            if w1[i][k].wtype == 2:
-                road_1[i][w1[i][k].pos]['Knight'] += 1
-            if w1[i][k].wtype == 3:
-                road_1[i][w1[i][k].pos]['Archer'] += 1
-        for k in range(len(w2[i])):
-            if w2[i][k].wtype == 2:
-                road_2[i][w2[i][k].pos]['Knight'] += 1
-            if w1[i][k].wtype == 3:
-                road_2[i][w2[i][k].pos]['Archer'] += 1
-        
-
-
-def print_soldier():
-    warrior_up.draw() 
-
-
-'''
+    # if order_command.CmdType > 0
 
 
 # 返回Command
@@ -177,24 +113,10 @@ def info():
     return order_command
 
 
-
-
-
-
-
-
-
 def update():
     global turnID
     turnID += 1
     draw()
-
-
-
-
-
-
-
 
 
 pgzrun.go()
