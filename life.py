adc547c6ac5f29664a373d5cdf590cbe705d2604
Chen-Yue-Zhao <67790570+Chen-Yue-Zhao@users.noreply.github.com>
2020-07-06 15:21:11 +0800

    Update life.py

--- life.py
+++ life.py
@@ -15,6 +15,8 @@ wanjia2=str("yyy")
 life0=Actor('血框')
 life1=Actor('血块')
 life2=Actor('生命值图标')
+life3=Actor('防御塔图标')
+life4=Actor('主塔图标')
 
 #这个生命值，随便编的名称随便设的数字，整合的时候要搞这里,
 # 0123分别是己方base和防御塔123,4567是敌方
@@ -26,26 +28,39 @@ def draw():
   
     for j in range(8):
         if j>3:
-            dfx=945
-            dfy=-400
+            dfx=0
+            dfy=-640
         else:
             dfx=0
             dfy=0
-        life0.topleft=25+dfx,300+j*70+dfy
-        life2.topleft=27+dfx,281+j*70+dfy
+        life0.topleft=25+dfx,440+j*70+dfy
+        life2.topleft=27+dfx,421+j*70+dfy
+        if j%4==0:
+            life4.topleft=210+dfx,416+j*70+dfy
+            life4.draw()
+            screen.draw.text("Base",(168+dfx,422+70*j+dfy))
+        else:
+            life3.topleft=214+dfx,416+j*70+dfy
+            life3.draw()
+            if (j-1)%4==0:
+                screen.draw.text("Turret(top)",(131+dfx,422+70*j+dfy))
+            elif (j-2)%4==0:
+                screen.draw.text("Turret(mid)",(131+dfx,422+70*j+dfy))
+            else:
+                screen.draw.text("Turret(bot)",(131+dfx,422+70*j+dfy))
         life0.draw()
         life2.draw()
         for i in range(lifezhi[j]):
-            life1.topleft=27+20*i+dfx,303+70*j+dfy
+            life1.topleft=27+20*i+dfx,443+70*j+dfy
             life1.draw()
-        screen.draw.text("Life:%d/10" % lifezhi[j],(50+dfx,282+70*j+dfy))
+        screen.draw.text("Life:%d/10" % lifezhi[j],(50+dfx,422+70*j+dfy))
 # 账号/昵称
-    renxiang.topleft=25,600
+    renxiang.topleft=27,380
     renxiang.draw()
-    renxiang.topleft=970,100
+    renxiang.topleft=27,20
     renxiang.draw()    
-    screen.draw.text("player: %s" % wanjia1,(50,602))
-    screen.draw.text("player: %s" % wanjia2,(995,102))
+    screen.draw.text("player: %s" % wanjia1,(52,382))
+    screen.draw.text("player: %s" % wanjia2,(52,22))
 
 #钱数(血量)变化的函数
 def lifezhibh(j,lfbh):
