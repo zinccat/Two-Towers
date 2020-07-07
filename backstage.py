f55bef9915ffcd49a1dea2c68fde18d181472fc2
Chen-Yue-Zhao <67790570+Chen-Yue-Zhao@users.noreply.github.com>
2020-07-07 22:28:59 +0800

    Update backstage.py

--- backstage.py
+++ backstage.py
@@ -18,7 +18,6 @@ from time import sleep
 
 # 此处定义了游戏所用到的全局变量
 
-
 #HOST = '65.49.209.247'
 
 HOST = 'localhost'
@@ -37,9 +36,7 @@ target = ['']
 
 game = ''
 
-
 # 连接到服务器并注册
-
 
 def connect():
 
@@ -73,15 +70,10 @@ def connect():
 class Command:
 
     """命令类
-
     包含四个参数:
-
     turnID:该命令所生效的回合
-
     CmdType:指令的类型,用于指导CmdStr的读取方式
-
     CmdStr:指令的内容，是一个列表
-
     """
 
     def __init__(self, turnID, CmdType, CmdStr):
@@ -141,6 +133,7 @@ def sendOp(target, op):
     #turnID, CmdType, CmdStr, optype
 
     dataObj = {'froms': userAccount, 'to': target,
+
                'turnID': op.turnID, 'CmdType': op.CmdType, 'CmdStr': op.CmdStr}
 
     datastr = json.dumps(dataObj)
@@ -157,29 +150,11 @@ def sendOp(target, op):
 class Command:
 
     """命令类
-
-
-
     包含四个参数:
-
-
-
     turnID:该命令所生效的回合
-
-
-
     # 已删除 直接归类到己方和敌方列表即可 playerID:发出该命令的玩家,可以取1 or 2
-
-
-
     CmdType:指令的类型,用于指导CmdStr的读取方式
-
-
-
     CmdStr:指令的内容，是一个列表
-
-
-
     """
 
     def __init__(self, turnID, CmdType, CmdStr):
@@ -208,9 +183,13 @@ class Battle:
         self.WarriorDefence = WarriorDefence
 
     def BattleGo(self):
+
         if self.WarriorDefence.wLife > 0:
+
             # 只有在对方没死的时候才会攻击并更新aCD
+
             self.WarriorAttack.updateaCD(1)
+
             self.WarriorDefence.wLife -= self.WarriorAttack.wAttack
 
 
@@ -235,6 +214,7 @@ class Action:
         self.w2 = [[], [], []]
 
         # 更新血量数值
+
         self.life = [0, 0, 0, 0, 0, 0, 0, 0]
 
         # 建立一个战斗列表
@@ -258,11 +238,17 @@ class Action:
         # 初始化对方主塔和防御塔
 
         for i in range(3):
+
             if i == 1:
+
                 self.w2[i].append(Base(2, 0, mLen))
+
                 self.w2[i].append(Turret(2,  2, mLen - dLen))
+
             else:
+
                 self.w2[i].append(Base(2, 0, aLen))
+
                 self.w2[i].append(Turret(2, 2, aLen - dLen))
 
     # 打钱!
@@ -308,50 +294,84 @@ class Action:
         # 金钱更新
 
         if self.money < 10:
+
             self.timeCount += 1
+
             if self.timeCount == 60:
+
                 self.MoneyAccumulate(1)
+
                 self.timeCount = 0
 
         # 武士状态刷新
 
         for i in range(3):
+
             for w in self.w1[i]:
+
                 w.attacked = False  # 重置攻击状态
+
                 w.updatemCD(0)  # 更新mCD
+
                 w.updateaCD(0)  # 更新aCD
+
                 if w.wType == 1:  # Turret
+
                     self.life[i + 1] = w.wLife
+
         for i in range(3):
+
             for w in self.w2[i]:
+
                 w.attacked = False  # 重置攻击状态
+
                 w.updatemCD(0)  # 更新mCD
+
                 w.updateaCD(0)  # 更新aCD
+
                 if w.wType == 1:  # Turret
+
                     self.life[i + 5] = w.wLife
 
     # 收取命令
 
     class getCmd(threading.Thread):
 
+        def __init__(self):
+
+            super().__init__(group=None, target=None, name=None, args=(), kwargs={}, daemon=None)
+
+            self.ops2 = None
+
+        # 为了处理ops2不存在的问题
+
         def run(self):
-            
+
             while True:
 
                 try:
+
                     data = tcpCliSock.recv(BUFSIZE).decode('utf-8')
+
                     if data == '-1':
+
                         print('can not connect to target!')
+
                         break
+
                     else:
+
                         dataObj = json.loads(data)
 
                         print('{} ->{} : {} {} {}'.format(
-                            dataObj['froms'], userAccount, dataObj['turnID'], dataObj['CmdType'], dataObj['CmdStr']))
-                        t = Command(
-                            dataObj['turnID'], dataObj['CmdType'], dataObj['CmdStr'])
-                        self.ops2.put(t)
 
+                            dataObj['froms'], userAccount, dataObj['turnID'], dataObj['CmdType'], dataObj['CmdStr']))
+
+                        t = Command(
+
+                            dataObj['turnID'], dataObj['CmdType'], dataObj['CmdStr'])
+
+                        self.ops2.put(t)
 
                 except:
 
@@ -368,36 +388,67 @@ class Action:
         genNum = 0  # 本回合生成了几个warrior?
 
         while(not self.ops1.empty()):
+
             tempOp = self.ops1.get()  # ops为命令队列
+
             if self.turnID == tempOp.turnID:
+
                 if tempOp.CmdType == 2:  # 骑士
+
                     genNum += 1
+
                     tempObj = Knight(1, genNum, 0)
+
                     self.w1[int(tempOp.CmdStr[0]-1)].append(tempObj)
+
                 if tempOp.CmdType == 3:  # 弓箭手
+
                     genNum += 1
+
                     tempObj = Archer(1, genNum, 0)
+
                     self.w1[int(tempOp.CmdStr[0]-1)].append(tempObj)
+
             else:
+
                 # 时机未到
+
                 self.ops1.put(tempOp)
+
                 break
+
         genNum = 0  # 本回合生成了几个warrior?
 
         while(not self.ops2.empty()):
+
             tempOp = self.ops2.get()  # ops为命令队列
+
             if self.turnID == tempOp.turnID:
+
                 if tempOp.CmdType == 2:  # 骑士
+
                     genNum += 1
-                    tempObj = Knight(2, genNum, mLen if self.CmdStr == 2 else dLen)
+
+                    tempObj = Knight(
+                        2, genNum, mLen if self.CmdStr == 2 else dLen)
+
                     self.w2[int(tempOp.CmdStr[0]-1)].append(tempObj)
+
                 if tempOp.CmdType == 3:  # 弓箭手
+
                     genNum += 1
-                    tempObj = Archer(2, genNum, mLen if self.CmdStr == 2 else dLen)
+
+                    tempObj = Archer(
+                        2, genNum, mLen if self.CmdStr == 2 else dLen)
+
                     self.w2[int(tempOp.CmdStr[0]-1)].append(tempObj)
+
             else:
+
                 # 时机未到
+
                 self.ops2.put(tempOp)
+
                 break
 
     # 士兵对战判断函数
@@ -433,54 +484,93 @@ class Action:
         sumAttack2 = 0
 
         for i in range(3):
+
             for w in self.w2[i]:
+
                 if w.wType == 0:
+
                     sumAttack2 += INF - w.wLife
+
                     break
+
         self.life[4] = max(0, TrueBaseLife - sumAttack2)
+
         if sumAttack2 >= TrueBaseLife:
+
             # 向玩家1显示ta胜利
+
             # 士兵阵亡函数
+
             return 1
 
         sumAttack1 = 0
 
         for i in range(3):
+
             for w in self.w1[i]:
+
                 if w.wType == 0:
+
                     sumAttack2 += INF - w.wLife
+
         self.life[0] = max(0, TrueBaseLife - sumAttack1)
+
         if sumAttack1 >= TrueBaseLife:
+
             # 向玩家2显示ta胜利
+
             return 2
+
         return 0
 
     def WarriorDeath(self):
+
         for i in range(3):
+
             for w in self.w1[i]:
+
                 if w.wLife <= 0:
+
                     if w.wType == 1:
+
                         print('防御塔被攻陷!')
+
                         self.life[i + 1] = 0
+
                     self.w1[i].remove(w)
+
         for i in range(3):
+
             for w in self.w2[i]:
+
                 if w.wLife <= 0:
+
                     if w.wType == 1:
+
                         print('防御塔被攻陷!')
+
                         self.life[i + 5] = 0
+
                     self.w2[i].remove(w)
 
     # 士兵移动函数
 
     def WarriorMove(self, WarriorList, posOccu, team):
+
         posDict = dict()
+
         # 第一轮扫描完成各位置上对象的计数
+
         for i in WarriorList:
+
             posDict[(i.pos, i.wGrid)] = True
+
             posOccu[i.pos] = team
+
         # 若前方有足够位置就前进, 先排序避免堵车
+
         WarriorList.sort(key=lambda Warrior: Warrior.pos,
+
                          reverse=(WarriorList[0].wTeam == 1))
 
         # 友方移动量为1
@@ -494,8 +584,11 @@ class Action:
             mov = -1
 
         for i in WarriorList:
-            if i.mCD == 0 and not i.attacked and i.wType > 1: # 塔不能跑!
+
+            if i.mCD == 0 and not i.attacked and i.wType > 1:  # 塔不能跑!
+
                 for j in range(1, 4):
+
                     if posDict.get((i.pos + mov, j), 0) == False and posOccu.get(i.pos + mov, team) == team:
 
                         posDict[(i.pos, i.wGrid)] = False
@@ -507,7 +600,9 @@ class Action:
                         posOccu[i.pos] = team
 
                         i.wGrid = j
+
                         i.updatemCD(1)
+
                         break
 
     def end(self, result):
@@ -522,86 +617,27 @@ class Action:
 
 
 '''
-
-
-
 # 命令测试
-
-
-
 w1 = [[], [], []]
-
-
-
 ops = queue.PriorityQueue()
-
-
-
 turnID = 1
-
-
-
 ops.put(Command(1, 2, [2]))
-
-
-
 ReadCmd(ops, w1, 1)
-
-
-
 print(w1)
-
-
-
 '''
 
 
 '''
-
-
-
 # WarriorMove测试代码
-
-
-
 l = []
-
-
-
 l.append(warrior.Archer(1, 2))
-
-
-
 l.append(warrior.Archer(1, 2))
-
-
-
 l.append(warrior.Archer(1, 2))
-
-
-
 l.append(warrior.Archer(1, 2))
-
-
-
 for i in range(1, 4):
-
-
-
     l[i].pos = mLen-1
-
-
-
-WarriorMove(l)
-
-
-
-for i in range(4):
-
-
-
+WarriorMove(l)for i in range(4):
     print(l[i].pos)
 
-
-
 '''
+
